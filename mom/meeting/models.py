608e50237172ef25df680bb87bee1a5cbdf4067d
devanghingu <43535635+devanghingu@users.noreply.github.com>
2020-03-03 19:48:23 +0530

    model created in meeting App

--- mom/meeting/models.py
+++ mom/meeting/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from mom.useractivity.models import 
+class Meeting(models.Model):
+    name        =   models.CharField(max_length=50,blank=True)
+    meetingtext =   models.TextField(blank=True)
+    user        =   models.ForeignKey(User, on_delete=models.CASCADE)
+    # participate =   models.ManyToManyField("app.Model", verbose_name=_(""))
 
-# Create your models here.
+class Notes(models.Model):
+    name        =   models.CharField(max_length=50,blank=True)
+    description =   models.TextField(blank=True)
+    meeting     =   models.ManyToManyField(Meeting,on_delete=models.CASCADE)
+    # subnotes    =   models.ForeignKey(Notes, on_delete=models.CASCADE)
